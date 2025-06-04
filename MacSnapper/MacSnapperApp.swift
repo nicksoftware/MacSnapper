420b4f7bf5f1103f825f3d7e8b7bb6c79975082d
nicksoftware <nicolusmaluleke@gmail.com>
2025-06-04 22:38:32 +0200

    Initial Commit

---
+++ MacSnapper/MacSnapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSnapperApp.swift
+//  MacSnapper
+//
+//  Created by HTCD on 2025/06/04.
+//
+
+import SwiftUI
+
+@main
+struct MacSnapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacSnapperTests/MacSnapperTests.swift
@@ -0,0 +1,16 @@
+//
+//  MacSnapperTests.swift
+//  MacSnapperTests
+//
+//  Created by HTCD on 2025/06/04.
+//
+
+import Testing
+
+struct MacSnapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
